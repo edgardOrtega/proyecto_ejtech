d5e5c87db8259b27c671cee03597d0775d03a89e
EdgardOrtegaPino <edgard.ortega.p@gmail.com>
2025-02-24 00:05:21 -0300

    agregar marco a imagenes

--- frontend/src/view/Ejtech.jsx
+++ frontend/src/view/Ejtech.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Card, Container, Row, Col } from "react-bootstrap";
 import imagenCreador1 from "../assets/edgard.png";
 import imagenCreador2 from "../assets/joaquin.png";
+
 const Ejtech = () => {
   return (
     <Container className="text-center p-5">
@@ -13,19 +14,19 @@ const Ejtech = () => {
       
       <Row className="justify-content-center mt-4">
         <Col md={4} className="d-flex justify-content-center">
-          <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={imagenCreador1} alt="Creador 1" />
-            <Card.Body>
-              <Card.Title>Edgard Ortega</Card.Title>
+          <Card style={{ width: '18rem', border: '2px solid #fefe00', boxShadow: '5px 5px 15px rgba(0, 0, 0, 0.2)' }}>
+            <Card.Img variant="top" src={imagenCreador1} alt="Creador 1" className="p-2 rounded" />
+            <Card.Body className="text-center">
+              <Card.Title className="fw-bold">Edgard Ortega</Card.Title>
             </Card.Body>
           </Card>
         </Col>
 
         <Col md={4} className="d-flex justify-content-center">
-          <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={imagenCreador2} alt="Creador 2" />
-            <Card.Body>
-              <Card.Title>joaquin Lopez</Card.Title>
+          <Card style={{ width: '18rem', border: '2px solid #fefe00', boxShadow: '5px 5px 15px rgba(0, 0, 0, 0.2)' }}>
+            <Card.Img variant="top" src={imagenCreador2} alt="Creador 2" className="p-2 rounded" />
+            <Card.Body className="text-center">
+              <Card.Title className="fw-bold">Joaquín</Card.Title>
             </Card.Body>
           </Card>
         </Col>
